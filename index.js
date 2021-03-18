9febd24e12d32c833c2a82031cdd56933fa8a0ba
NataJenkins <38480860+NataJenkins@users.noreply.github.com>
2021-03-18 15:28:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let myLibrary = [];
 
 function Book(title, author) {
-  this.title = title
-  this.author = author
+  this.title = title;
+  this.author = author;
 }
 
 function addBookToLibrary(book, library) {
-    library.push(book)
+  library.push(book);
 }
 
-let book1 = new Book('Math', 'Math Guy')
-let book2 = new Book('Spanish', 'Spanish Guy')
-let book3 = new Book('Azul', 'Ruben Dario')
-let book4 = new Book('Illuminations', 'Arthur Rimbaud')
+let book1 = new Book("Math", "Math Guy");
+let book2 = new Book("Spanish", "Spanish Guy");
+let book3 = new Book("Azul", "Ruben Dario");
+let book4 = new Book("Illuminations", "Arthur Rimbaud");
 
 addBookToLibrary(book1, myLibrary);
 addBookToLibrary(book2, myLibrary);
@@ -20,8 +20,13 @@ addBookToLibrary(book3, myLibrary);
 addBookToLibrary(book4, myLibrary);
 
 for (let i = 0; i < myLibrary.length; i++) {
-    let bookElement = document.createElement('div');
-    bookElement.textContent = `${myLibrary[i].title} | ${myLibrary[i].author}`
-    document.body.appendChild(bookElement);  
-    // console.log(`${myLibrary[i].title} | ${myLibrary[i].author}`)
+  let bookElement = document.createElement("div");
+  let bookAuthor = document.createElement("div");
+  bookElement.classList.add("title");
+  bookElement.textContent = `${myLibrary[i].title}`;
+  bookAuthor.classList.add("author");
+  bookAuthor.textContent = `${myLibrary[i].author}`;
+  document.body.appendChild(bookElement);
+  document.body.appendChild(bookAuthor);
+  // console.log(`${myLibrary[i].title} | ${myLibrary[i].author}`)
 }
